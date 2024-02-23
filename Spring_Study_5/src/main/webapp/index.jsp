b02ed49f2aa526bf2630333fff8b93826a871e38
JinseoKim <eunoia@handong.ac.kr>
2024-02-23 10:56:36 +0900

    Third commit

---
+++ Spring_Study_5/src/main/webapp/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: psi021
+  Date: 2/21/24
+  Time: 8:02 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+    <div>Index</div>
+</body>
+</html>
---
+++ Spring_Study_7/src/main/java/org/example/model/Customer.java
@@ -0,0 +1,65 @@
+package org.example.model;
+
+import org.hibernate.validator.constraints.Email;
+
+import javax.validation.constraints.*;
+
+public class Customer {
+    private String firstName;
+
+    @NotNull(message = "is required")
+    @Size(min = 1, message = "is required")
+    private String lastName;
+
+    @NotNull(message = "is required")
+    @Min(value = 0, message = "must be greater than or equal to zero")
+    @Max(value = 10, message = "must be less than or equal to ten")
+    private String freePasses;
+
+    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 chars/digits")
+    private String postalCode;
+
+    @NotNull(message = "is required")
+    @Email(message = "Invalid email! Please enter valid email")
+    private String email;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getFreePasses() {
+        return freePasses;
+    }
+
+    public void setFreePasses(String freePasses) {
+        this.freePasses = freePasses;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
